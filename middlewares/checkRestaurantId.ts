f6ad812d2fa23f19f6d365d56fd31c473319d08d
Rahulkrishnan R <rahul.krishnan@talenq.com>
2026-02-10 20:28:43 +0530

    hashes

---
+++ middlewares/checkRestaurantId.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import { initializeRedisClient } from "../utils/client.js";
+import { restaurantKeyById } from "../utils/keys.js";
+import { errorResponse } from "../utils/responses.js";
+
+export const checkRestaurantExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { restaurantId } = req.params;
+  if (!restaurantId) {
+    return errorResponse(res, 400, "Restaurant ID not found");
+  }
+  const client = await initializeRedisClient();
+  const restaurantkey = restaurantKeyById(restaurantId);
+  const exists = await client.exists(restaurantkey);
+  if (!exists) {
+    return errorResponse(res, 400, "Restaurant not found");
+  }
+  next();
+};
